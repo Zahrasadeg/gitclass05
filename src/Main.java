618515e2c0261dff6445ca020d091d15d01d5458
Zahrasadeg <sadeghiz071@gmail.com>
2023-03-26 14:06:06 -0400

    seconed

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,8 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
     }
 }
